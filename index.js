45b3f3af39d535f24283cde01bb85d4e3c9eb4fe
duy <duy@duys-MacBook-Pro.local>
2020-06-06 23:28:09 +0700

    s;;;

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views',__dirname + './views');
-app.use('/static', express.static('public'));
+app.use(express.static(__dirname + 'public'));
 
 app.get('/', (req, res) => {
   res.render('home');
