4e193e82e07621e9abdd050f1f55e37e695755d6
duy <duy@duys-MacBook-Pro.local>
2020-06-06 23:31:51 +0700

    oooo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.set('port', 4000 || process.env.PORT, () => console.log('start server'));
+
 app.set('view engine', 'ejs');
 app.set('views',__dirname + './views');
 app.use(express.static(__dirname + 'public'));
